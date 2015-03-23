075a0d96372d2b768334349d73dbdc85572141e9
durgeshb <durgesh.bhargava@uconn.edu>
2015-03-22 23:03:14 -0400

    First commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# next I'll make sure that all my programs (written in Python in this example) have the proper permissions
+chmod a+x /home/src/word_count_median.py
+
+# finally I'll execute my programs
+python ./src/word_count_median.py
